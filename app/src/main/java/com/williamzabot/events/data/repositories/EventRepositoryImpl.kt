55111a613c2f63ed65b916e572e32de5fde4a925
williamcansizabot <william.zabot@eldorado.org.br>
2021-04-13 09:57:39 -0300

    ajuste

--- app/src/main/java/com/williamzabot/events/data/repositories/EventRepositoryImpl.kt
+++ app/src/main/java/com/williamzabot/events/data/repositories/EventRepositoryImpl.kt
@@ -3,8 +3,8 @@ package com.williamzabot.events.data.repositories
 import com.williamzabot.events.data.di.RetrofitModule.provideEventApi
 import com.williamzabot.events.data.model.EventDTO
 import com.williamzabot.events.domain.repositories.EventRepository
-import com.williamzabot.events.domain.utils.CREATED
 import com.williamzabot.events.domain.utils.Result
+import com.williamzabot.events.domain.utils.SUCCESS
 import javax.inject.Inject
 
 class EventRepositoryImpl @Inject constructor() : EventRepository {
@@ -14,7 +14,7 @@ class EventRepositoryImpl @Inject constructor() : EventRepository {
     override suspend fun getEvents(): Result<List<EventDTO>> {
         val response = eventApi.getEvents()
         return when (response.code()) {
-            CREATED -> Result.Success(response.body()!!)
+            SUCCESS -> Result.Success(response.body()!!)
             else -> Result.Failure(Exception())
         }
     }
--- app/src/main/java/com/williamzabot/events/domain/utils/Constants.kt
+++ app/src/main/java/com/williamzabot/events/domain/utils/Constants.kt
@@ -3,3 +3,4 @@ package com.williamzabot.events.domain.utils
 const val BASE_URL = "http://5f5a8f24d44d640016169133.mockapi.io/api/"
 const val CREATED = 201
 const val BAD_REQUEST = 400
+const val SUCCESS = 200
